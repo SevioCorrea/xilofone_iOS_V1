3ef8f3bab8ca4b9d21eceece3f328143aa0e7a71
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2022-06-18 18:29:44 -0300

    xilofone_iOS

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Sévio on 18/06/2022.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        print(sender.currentTitle)
+        
+        playSound()
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
+
+
